c2d1db64dcab725b3428b69781822ab0dd3c7cc5
Toshiyuki Yokoyama <6br@users.noreply.github.com>
2019-08-26 10:48:19 +0900

    Create profile.ts

---
+++ src/profile.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import Neo4JHandler from "./neo4j";
+
+var ConfigFile = require('config');
+var router = express.Router()
+
+/**
+ * @swagger
+ * /profile:
+ *   get:
+ *     summary: "Graph Profile"
+ *     tags:
+ *       - name: Profile
+ *     description: Get data profiling of graphs
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: success
+ */
+
+router.get('', function (req: Request, res: Response) {
+    switch (ConfigFile.db.dbms) {
+        case "neo4j":
+            Neo4JHandler.profile_graph(req, res)
+            break;
+    
+        default:
+            break;
+    }
+})
+
+module.exports = router
