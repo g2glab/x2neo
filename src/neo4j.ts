4deabb81305a537048731c74f0c5408221ca28df
Toshiyuki Yokoyama <6br@users.noreply.github.com>
2020-05-30 23:24:27 +0900

    Update neo4j.ts

--- src/neo4j.ts
+++ src/neo4j.ts
@@ -18,7 +18,7 @@ function shortest_cypher(query: any, k: number | string, m: number): string {
     const to_node_label = query.to_node_label ? ":" + query.to_node_label : "" ; //":" + "airport"
     const to_node_props = JSON.stringify(to_node_props_hash).replace(/\"([^(\")"]+)\":/g,"$1:").replace(/\\"/g, '\\"'); // Remove double quotes on keys
     const edge_label = query.edge_label ? ":" + query.edge_label : "*"; // "has_flight_to"
-    let iteration = "*";
+    let iteration = edge_label == "*" ? "" : "*";
     if (k !== "*" || m !== 0) {
         iteration = m.toString() + ".." + k.toString()
     }
